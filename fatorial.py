91348250fb52d32558eb5b9aa43f3151739b5e2e
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-11-23 11:16:57 -0300

    Reestruturação do projeto, separando arquivo de testes

--- fatorial.py
+++ fatorial.py
@@ -1,32 +1,6 @@
 from functools import reduce
 from operator import mul
 
-import pytest
-
 
 def fatorial(numero):
     return reduce(mul, range(1, numero + 1)) if numero >= 1 else 1
-
-
-def test_fatorial_quatro():
-    assert fatorial(4) == 24
-
-
-def test_fatorial_tres():
-    assert fatorial(3) == 6
-
-
-def test_fatorial_dois():
-    assert fatorial(2) == 2
-
-
-def test_fatorial_zero():
-    assert fatorial(0) == 1
-
-
-def test_fatorial_um():
-    assert fatorial(1) == 1
-
-
-if __name__ == '__main__':
-    pytest.main([__file__, '--verbose'])
---
+++ test_fatorial.py
@@ -0,0 +1,21 @@
+from fatorial import fatorial
+
+
+def test_fatorial_quatro():
+    assert fatorial(4) == 24
+
+
+def test_fatorial_tres():
+    assert fatorial(3) == 6
+
+
+def test_fatorial_dois():
+    assert fatorial(2) == 2
+
+
+def test_fatorial_zero():
+    assert fatorial(0) == 1
+
+
+def test_fatorial_um():
+    assert fatorial(1) == 1
