20ab51deaea2c996c0f74dc8db420bae7f0e952a
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-11-25 08:50:50 -0300

    Primeira versão CLI

--- fatorial.py
+++ fatorial.py
@@ -4,3 +4,16 @@ from operator import mul
 
 def fatorial(numero):
     return reduce(mul, range(1, numero + 1)) if numero >= 1 else 1
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            n = int(input('Forneça um número inteiro: '))
+        except ValueError:
+            print('ERRO: Forneça um número inteiro positivo.')
+        except KeyboardInterrupt:
+            print(' Excecução finalizada pelo usuário.')
+            break
+        else:
+            print(fatorial(n))
